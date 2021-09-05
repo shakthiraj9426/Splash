3b78816f20281e02acbbf9f891d2b32eab1b22a9
Ammyy9908 <sb78639@gmail.com>
2021-09-05 22:28:14 +0530

    bug fixed!

--- js/script.js
+++ js/script.js
@@ -393,7 +393,7 @@ x.play();
 
   /*Weather Funtion start from here*/
   function getWeather(city) {
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=16020d8d307b65395a579b8cdb9b33dd&units=metric";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=10b151862c9f7cf37cd60d00eeeec53c&units=metric";
 
 
     $.ajax({
